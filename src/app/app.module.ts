255c100d4db6c5cc0cef87c01b221fc99801e78a
Nihat Zaman <iceweasel@Nihats-MacBook-Air.local>
2023-08-11 17:58:21 +0300

    header eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { FooterComponent } from './footers/footer/footer.component';
 import { LinkComponent } from './link/link.component';
 import { ButtonComponent } from './button/button.component';
 import { LogoComponent } from './logo/logo.component';
 import { BannerComponent } from './banner/banner.component';
 import { WidgetComponent } from './widget/widget.component';
 import { CardComponent } from './card/card.component';
-import { NavbarTopComponent } from './navbar-top/navbar-top.component';
-import { FooterTopComponent } from './footer-top/footer-top.component';
+import { NavbarTopComponent } from './header/navbar-top/navbar-top.component';
+import { FooterTopComponent } from './footers/footer-top/footer-top.component';
 
 @NgModule({
   declarations: [
@@ -26,13 +26,10 @@ import { FooterTopComponent } from './footer-top/footer-top.component';
     WidgetComponent,
     CardComponent,
     NavbarTopComponent,
-    FooterTopComponent
+    FooterTopComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
