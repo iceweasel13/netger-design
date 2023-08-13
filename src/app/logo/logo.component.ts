71fc63776cec7f69abbac22f9b1c2145304c948d
Fırat Yesilgöz <56675568+firatyesilgoz@users.noreply.github.com>
2023-08-13 21:02:13 +0300

    logo ve footer değişiklik

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent {
-
+  logo: string[] = [
+    "/assets/edepola.svg",
+    "/assets/tatildükkan.png",
+    "/assets/akselim.png",
+    "/assets/Incehesap-logo.png",
+    "/assets/modasaat.png",
+    "/assets/novapazar.png",
+    "/assets/44427.png",
+    "/assets/ejzane.png",
+    "/assets/indir.png",
+    "/assets/ortaklar.png",
+    "/assets/Technolat_Logo.png",
+    "/assets/konusarak.png",
+    "/assets/gettap.png",
+    "/assets/usc.png",
+    "/assets/polat.png",
+    "/assets/skynak.png",
+    "/assets/buajans.png",
+    "/assets/erzincan.png",
+    "/assets/istanbulbüro.png",
+    "/assets/ews.png",
+    "/assets/uyafa.png",
+    "/assets/loft.png",
+    "/assets/atasever.png",
+    "/assets/ekram.png",
+    "/assets/tuze.png",
+    
+    
+  ];
 }
