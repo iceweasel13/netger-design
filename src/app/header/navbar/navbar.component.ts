dd3e5912787806f98ed510b4cebdb3aace378833
Nihat Zaman <iceweasel@Nihats-MacBook-Air.local>
2023-08-17 15:22:21 +0300

    header tamamlandi

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-
-}
+export class NavbarComponent {}
